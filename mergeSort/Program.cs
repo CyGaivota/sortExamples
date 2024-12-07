12073f6d1465656ce9e82ff8042bc67eefd81dd6
Maren <gaivota@windowslive.com>
2024-12-07 13:27:31 +0100

    Add project files.

---
+++ mergeSort/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace mergeSort
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            // Beispiel-Datenmenge
+            int[] array = { 34, 7, 23, 32, 5, 62 };
+
+            // Eingabedaten ausgeben
+            Console.WriteLine("Eingabedaten:");
+            PrintArray(array);
+
+            // Mergesort ausführen
+            Mergesort(array, 0, array.Length - 1);
+
+            // Ausgabedaten ausgeben
+            Console.WriteLine("Sortierte Ausgabedaten:");
+            PrintArray(array);
+        }
+
+        static void Mergesort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+                Mergesort(array, left, middle);
+                Mergesort(array, middle + 1, right);
+                Merge(array, left, middle, right);
+            }
+        }
+
+        static void Merge(int[] array, int left, int middle, int right)
+        {
+            int leftSize = middle - left + 1;
+            int rightSize = right - middle;
+            int[] leftArray = new int[leftSize];
+            int[] rightArray = new int[rightSize];
+
+            Array.Copy(array, left, leftArray, 0, leftSize);
+            Array.Copy(array, middle + 1, rightArray, 0, rightSize);
+
+            int i = 0, j = 0, k = left;
+
+            while (i < leftSize && j < rightSize)
+            {
+                if (leftArray[i] <= rightArray[j])
+                {
+                    array[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = rightArray[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < leftSize)
+            {
+                array[k] = leftArray[i];
+                i++;
+                k++;
+            }
+
+            while (j < rightSize)
+            {
+                array[k] = rightArray[j];
+                j++;
+                k++;
+            }
+        }
+
+        static void PrintArray(int[] array)
+        {
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+
+}
---
+++ quickSort/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace quickSort
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            // Beispiel-Datenmenge
+            int[] array = { 34, 7, 23, 32, 5, 62 };
+
+            // Eingabedaten ausgeben
+            Console.WriteLine("Eingabedaten:");
+            PrintArray(array);
+
+            // Quicksort ausführen
+            Quicksort(array, 0, array.Length - 1);
+
+            // Ausgabedaten ausgeben
+            Console.WriteLine("Sortierte Ausgabedaten:");
+            PrintArray(array);
+        }
+
+        static void Quicksort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(array, left, right);
+                Quicksort(array, left, pivotIndex - 1);
+                Quicksort(array, pivotIndex + 1, right);
+            }
+        }
+
+        static int Partition(int[] array, int left, int right)
+        {
+            int pivot = array[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (array[j] < pivot)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+
+            Swap(array, i + 1, right);
+            return i + 1;
+        }
+
+        static void Swap(int[] array, int a, int b)
+        {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+
+        static void PrintArray(int[] array)
+        {
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+
+}
